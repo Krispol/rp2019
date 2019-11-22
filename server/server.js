b3cec2be47bc2516b287c11be7d81c6d3a93df1f
Krispol <kristjan.poldmets@gmail.com>
2019-11-22 04:28:30 +0200

    Lesson 5: MongoDB Post item, get items

--- server/server.js
+++ server/server.js
@@ -2,52 +2,13 @@ const express = require('express');
 const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3000;
-const DB = require("./database.js");
 const mongoose = require("mongoose");
 require('dotenv').config();
-
-
-var kittySchema = new mongoose.Schema({
-    name: String
-});
-
-var Kitten = mongoose.model('Kitten', kittySchema);
-
-const kitten1= new Kitten({
-    name: "red cat 3"
-});
-
+const userRouter = require("./user.js");
 
 const DB_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0-w8n4s.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
-
-mongoose.connect(DB_URL)
-    .then(() => {
-        console.log("Database access success!")
-        kitten1.save( err =>{
-            if(err){
-                console.error("we had errorz");
-            }
-            else{
-                console.log("successful save")
-            }
-        });
-    })
-    .catch( err =>{
-        console.log("Error happenze" , err)
-    })
-
-app.get("/api/items", (req, res)=>{
-    res.json(DB.getItems())
-});
-
-app.get("/api/items/:itemId", (req,res)=>{
-    res.send(DB.getItem(req.params.itemId));
-});
-
-app.get("/hello", (req, res)=>{
-    res.send("post hello");
-});
+app.use(userRouter);
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, "../dist", "index.html"));
@@ -55,7 +16,18 @@ app.get('/', (req, res) => {
 
 app.use(express.static('dist'));
 
-app.listen(PORT, () => {
-    console.log("Server started", PORT);
-    console.log(`http://localhost:${PORT}`);
-});
+function listen(){
+    app.listen(PORT, () => {
+        console.log("Server started", PORT);
+        console.log(`http://localhost:${PORT}`);
+    });
+}
+
+mongoose.connect(DB_URL)
+    .then(() => {
+        console.log("Database access success!")
+        listen() //kui andmebaasi ühendus on loodud, siis kuulan serverit
+    })
+    .catch( err =>{
+        console.log("Error happenze" , err)
+    });
